11f23c26c98b77336eb0ab1388480668fbf04fbc
KarunyaRam <sprkarunya986@gmail.com>
2026-06-21 12:38:52 +0530

    feat: build quiz engine with Gemini-generated questions and user response handling

--- app/(main)/interview/_components/Quiz.jsx
+++ app/(main)/interview/_components/Quiz.jsx
@@ -1,8 +1,233 @@
-import React from 'react'
+"use client";
+import { generateQuiz, saveQuizResult } from '@/actions/interview';
+import { useFetch } from '@/app/hooks/useFetch';
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Label } from '@/components/ui/label';
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
+import { Loader2 } from 'lucide-react';
+import React, { useEffect, useState } from 'react'
+import { BarLoader } from 'react-spinners';
+import { toast } from 'sonner';
 
 const Quiz = () => {
+    const [currentQuestion,setCurrentQuestion]=useState(0);
+    const [answers,setAnswers]=useState([]); //! Tracks Array user selected answers
+    const [showExplanation,setShowExplanation]=useState(true)
+
+    const startNewQuiz =()=>{
+        setCurrentQuestion(0);
+        setAnswers([]);
+        setShowExplanation(false);
+        generateQuizFn();
+        setResultData(null) //!Coming From saveQuizResult Server Action
+    }
+    const {loading:generatingQuiz,
+        fn:generateQuizFn,
+        data:quizData,
+    } = useFetch(generateQuiz);
+    
+    const {loading:savingResult,
+        fn:saveQuizResultFn,
+        data:resultData,
+        setData:setResultData,
+    } = useFetch(saveQuizResult);
+
+    console.log(resultData);
+    
+
+    
+
+
+    //! quizData example response by GEMINI AI
+    //!   {
+//!  question: "What is React primarily used for?",
+//! options: [
+//!   "Database Management",
+//!   "Building User Interfaces",
+//!  "Operating Systems",
+//!    "Network Security"
+//! ],
+//!  correctAnswer: "Building User Interfaces",
+//!  explanation:
+//!   "React is a JavaScript library used for building interactive user interfaces."
+//! }
+
+
+    useEffect(()=>{
+        //!Set answers to null If the quizData is updated
+        if(quizData){
+            setAnswers(new Array(quizData.length).fill(null));
+        }
+    },[quizData]);
+
+
+    if(generatingQuiz){  //! If quiz is loading, show Loader 
+        return <BarLoader className='mt-4' width={"100%"} color='gray'/>
+    }
+
+    //! Show Results If quiz is completed
+    //!Also Show the Quiz Results Interface
+    if(resultData){
+        return(
+            <div className='mx-2'>
+                <QuizResult result={resultData} onStartNew={startNewQuiz}/>
+            </div>
+        )
+    }
+
+    if(!quizData){
+        return(
+            <Card>
+          <CardHeader>
+            <CardTitle>Ready to test your knowledge?</CardTitle>
+            <CardDescription>Skills to consider developing</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p className='text-muted-foreground'>
+                This quiz contains 10 questions specific to your industry and skills. 
+                Take your time and choose the best answer for each question
+
+            </p>
+          </CardContent>
+          <CardFooter>
+            <Button onClick={generateQuizFn} className={"w-full"}>
+            Start Quiz
+            </Button>
+          </CardFooter>
+        </Card>
+        )
+    }
+    const handleAnswer=(answer)=>{
+        //!As User is not selected any answer
+        //! Answers array contains initially as [null,null,.. etc]
+        const newAnswers = [...answers]
+        newAnswers[currentQuestion]=answer;
+        //!Once Option selected by the User, Array will be updated
+        //! Updates the answers array with currently selected answer
+        //! by user for the Current Quesion
+        setAnswers([newAnswers]);
+
+    }
+    const question = quizData[currentQuestion];
+    //! currentQuestion variable tracks index of Question
+    //! Goes as quizData[0]; {} first Question 
+
+    const handleNext=()=>{
+        //! On clicking Next, check the current question is last
+        //! If it's the last question then execute finishQuiz Function
+        if(currentQuestion < quizData.length-1){
+            //! Set the Next Question 
+            setCurrentQuestion(currentQuestion+1)
+            setShowExplanation(false);
+        } else{
+            finishQuiz();
+        }
+    }
+
+    const calculateScore=()=>{
+        let correct=0;
+        answers.forEach((answer,index)=>{
+            if(answer==quizData[index].correctAnswer){
+                correct++;
+            }
+        })
+        return correct/quizData.length *100;
+    }
+    
+    
+    const finishQuiz=async()=>{
+        //! We will calculate the score of the Quiz in this function
+        //! Save the Quiz Results to the Database 
+        //! using the server Action saveQuizResult
+        //!This will take parameters as questions,answers,score
+
+        const score=calculateScore();;
+        try {
+            await saveQuizResultFn(quizData,answers,score);
+            toast.success("Quiz Completed");
+        } catch (error) {
+            toast.error("Failed to Save the Quiz Results")  
+        }
+
+
+    }
+
+     
   return (
-    <div>Quiz</div>
+    <div>
+        <Card className={'mx-2'}>
+          <CardHeader>
+            <CardTitle>
+                Question {currentQuestion + 1} of {quizData.length}
+            </CardTitle>
+          </CardHeader>
+          <CardContent className={"space-y-4"}>
+            <p className='text-lg font-medium'>
+                {question.question}
+            </p>
+
+            <RadioGroup 
+            
+            onValueChange={handleAnswer}
+            value={answers[currentQuestion]}
+            className={"space-y-2"}
+            
+            >
+
+                {question.options.map((option,index)=>(
+                    <div className="flex items-center space-x-2" key={index}>
+                <RadioGroupItem  value={option} id={`option-${index}`}/>
+                <Label  htmlFor={`option-${index}`}>{option}</Label>
+            </div>
+                ))}
+            
+            </RadioGroup>
+
+            {showExplanation && (
+                <div className='mt-4 p-4 bg-muted rounded-lg'>
+                    <p className='font-medium'>Explanation:</p>
+                    <p className='text-muted-foreground'>{question.explanation}</p>
+
+                </div>
+            )}
+            
+          </CardContent>
+          <CardFooter>
+             //!If we have not selected show Selection till now, 
+             //!then we will show the 'Show Explanation button'
+             {!showExplanation &&(
+                <Button 
+                onClick={()=>setShowExplanation(true)}
+                variant='outline'
+                //! The answers array is not updated it means
+                //! the answer is not selected by user
+                //! Until user selects his answer don't show the explanation
+                disabled={!answers[currentQuestion]} 
+                
+                >
+                    Show Explanation
+                </Button>   
+
+             )}
+
+             <Button
+             onClick={handleNext}
+             className={'ml-auto'}
+             disabled={!answers[currentQuestion] || savingResult}
+             >
+                {savingResult && (
+                    <Loader2 className='mr-2 h-4 w-4 animate-spin'/>
+                )}
+
+                {currentQuestion < quizData.length-1 ? 
+                "Next Question" : "Finish Quiz"}
+             </Button>
+
+          </CardFooter>
+        </Card>
+    </div>
   )
 }
 
--- app/(main)/interview/mock/page.jsx
+++ app/(main)/interview/mock/page.jsx
@@ -7,7 +7,7 @@ import Quiz from '../_components/Quiz'
 const MockInterviewPage = () => {
   return (
     <>
-     <div>
+     <div className='container mx-auto space-y-4 py-6'>
       <div className='flex flex-col space-y-2 mx-2'>
         <Link href={"/interview"}>
         <Button variant='link' className={"gap-2 pl-0"}>
@@ -18,7 +18,7 @@ const MockInterviewPage = () => {
 
         <div>
            <h1 className="text-6xl font-bold gradient-title">Mock Interview</h1>
-          <p className="text-muted-foreground">
+          <p className="text-muted-foreground mb-5">
             Test your knowledge with industry-specific questions
           </p>
         </div>
